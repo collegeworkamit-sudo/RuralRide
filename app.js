59f679b98c3a6c449fe933ad54897bba59225049
collegeworkamit-sudo <collegeworkamit@gmail.com>
2026-04-16 22:05:30 +0000

    cors

--- app.js
+++ app.js
@@ -22,13 +22,16 @@ const server = http.createServer(app);
 // Socket.io setup
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://rural-ride-psi.vercel.app'],
     methods: ['GET', 'POST'],
   },
 });
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://rural-ride-psi.vercel.app'],
+  credentials: true,
+}));
 app.use(express.json());
 
 // Initialize simulator (will be started via API)
